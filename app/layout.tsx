e9fccd9271d90493df5ec7ff65dca5640ea0d874
Nikhil Kumawat <nikhilkumawat002@gmail.com>
2026-06-12 18:23:21 +0530

    check

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter, Montserrat } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { WishlistProvider } from "@/context/WishlistContext";
 import { CartProvider } from "@/context/CartContext";
 import FloatingWhatsappButton from "@/components/Whatsapp_Button/FloatingWhatsappButton";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
-
-const montserrat = Montserrat({
-  variable: "--font-montserrat",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -37,15 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`
-    ${geistSans.variable}
-    ${geistMono.variable}
-    ${inter.variable}
-    ${montserrat.variable}
-  `}
-    >
+    <html lang="en">
       <body className="min-h-screen  font-[var(--font-montserrat)]">
         <CartProvider>
           <WishlistProvider>
